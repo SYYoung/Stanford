e99b981336ef99d63c028281903ae19793006f19
r38411 <r38411@yahoo.com>
2019-06-15 16:38:58 +0800

    update ex1

--- Machine Learning/machine-learning-ex1/ex1/computeCost.m
+++ Machine Learning/machine-learning-ex1/ex1/computeCost.m
@@ -13,9 +13,9 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
-
-
+h_theta = X * theta;
+err = h_theta - y;
+J = sum(err .* err)/(2*m);
 
 % =========================================================================
 
--- Machine Learning/machine-learning-ex1/ex1/gradientDescent.m
+++ Machine Learning/machine-learning-ex1/ex1/gradientDescent.m
@@ -17,16 +17,14 @@ for iter = 1:num_iters
     %       of the cost function (computeCost) and gradient here.
     %
 
-
-
-
-
-
+    err = X * theta - y;
+    theta = theta - alpha/m * (err' * X)';
 
     % ============================================================
 
     % Save the cost J in every iteration    
     J_history(iter) = computeCost(X, y, theta);
+
 
 end
 

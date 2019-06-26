9ffa5564953ac8fc95763430efce7905d5f9b5b4
SY Young <r38411@yahoo.com>
2019-06-26 14:59:57 +0800

    update ex3

--- Machine Learning/machine-learning-ex3/ex3/predict.m
+++ Machine Learning/machine-learning-ex3/ex3/predict.m
@@ -20,15 +20,32 @@ p = zeros(size(X, 1), 1);
 %       information see 'help max'. If your examples are in rows, then, you
 %       can use max(A, [], 2) to obtain the max for each row.
 %
+m = size(X, 1);
+n = size(X, 2);
 
+% You need to return the following variables correctly 
+% all_theta = zeros(num_labels, n + 1);
 
+% Add ones to the X data matrix
+a_1 = [ones(m, 1) X]';
+fprintf("dim of a_1 = %d, %d", size(a_1,1), size(a_1,2));
 
+z_2 = Theta1 * a_1;
+fprintf("dim of z_2 = %d, %d", size(z_2,1), size(z_2,2));
+a_2 = sigmoid(z_2);
+fprintf("dim of a_2 = %d, %d", size(a_2,1), size(a_2,2));
+a_2 = [ones(size(a_2,2)); a_2];
+fprintf("dim of new a_2 = %d, %d", size(a_2,1), size(a_2,2));
 
+z_3 = Theta2 * a_2;
+fprintf("dim of z_3 = %d, %d", size(z_3,1), size(z_3,2));
+a_3 = sigmoid(z_3);
+fprintf("dim of a_3 = %d, %d", size(a_3,1), size(a_3,2));
 
-
-
-
-
+p1 = max(a_3, [], 1);
+p = p1';
+fprintf("dim of p = %d, %d", size(p,1), size(p,2));
+fprint("first 5 of p = %d, %d, %d, %d, %d", p(1:5,1));
 % =========================================================================
 
 
